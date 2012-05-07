423bbe3394b36d959263fb5747162b4714d428cf
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-05-07 04:48:04 -0700

    Cleaned up EM hubsocket

--- lib/WebSocketDaemon/eventmachine-websocket.rb
+++ lib/WebSocketDaemon/eventmachine-websocket.rb
@@ -2,6 +2,39 @@ require 'em-websocket'
 require 'json'
 
 
+class WebSocketEventMachine 
+  @connected = false
+  @request   = EM::Chanell.new
+  def receive_data data
+    
+    if @connected
+      #parse each frame and dispatch
+      while msg = data.slice!(Pack)
+        on_receive msg.gsub(Frame, "")
+      end
+    else        
+      if data =~ /\A<policy-file-request\s*\/>/
+        Log.debug "Sending flash policy #{self.class.policy_content}"
+        send_data self.class.policy
+        close_connection_after_writing
+      else
+        handshake data
+      end
+    end
+  end
+
+  def handshake data
+    begin
+      @request = Request.new(data)
+      send_data @request.response
+      on_ready
+    rescue Exception => ex
+      on_error ex
+      close_connection
+    end
+  end
+end
+
 EventMachine.run {
   @hub = EM::Channel.new
 
