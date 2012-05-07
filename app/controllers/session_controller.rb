7324a6c1a429d606f52a7e0d26263177ee9d4717
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-05-07 03:04:06 -0700

    Added a /grant_token route and fixed some session controller stuff

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,9 @@ require 'yaml'
 require 'digest/sha1'
 
 class SessionController < ApplicationController
+  def grant_token
+    redirect_to '/auth/google_oauth2'
+  end
   def create
     auth = request.env["omniauth.auth"]
 
@@ -13,14 +16,11 @@ class SessionController < ApplicationController
                       :google_token  => auth[:credentials]["token"],
                       :google_uid => auth[:info]["google_uid"])
       @user.save
-    end
-
-    unless @user.token
       token = Token.create(:user_id => @user.id, :token => Digest::SHA1.hexdigest(@user.google_token))
     end
 
     session[:user_id] = @user.id
-    render :text => @user.token.token 
+    render :text => "Your API Token #{@user.token.token}"
   end
 
   def destroy
