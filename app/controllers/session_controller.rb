43449c90127e14f983403be9c504a325dd1f050b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-05-07 01:06:03 -0700

    Fixed up session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,16 @@
 require 'yaml'
+require 'digest/sha1'
 
 class SessionController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.new(:name   => auth[:info]["name"],
+                    :email  => auth[:info]["email"], 
+                    :google_token  => auth[:info]["token"],
+                    :google_uid => auth[:info]["google_uid"])
+    user.save
+    token = Token.new(:user_id => user.id, :token => Digest::SHA1.hexdigest(user.google_token))
+
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
